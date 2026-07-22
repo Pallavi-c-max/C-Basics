b22ac17f0b78041e8978f26cbc61d1c305d25d33
Pallavi <pallaviyericherla@gmail.com>
2026-07-22 23:10:49 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+      int a,b,sum;
+      
+      printf("Enter value for a :");
+      scanf("%d",&a);
+     
+      printf("Enter value for b :");
+      scanf("%d",&b);
+      
+      sum = a + b;
+     
+      printf("Sum = %d",sum);
+
+      return 0;
+}
