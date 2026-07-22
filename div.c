2db615641cf11f317791c6c656c92532f018cd9a
Pallavi <pallaviyericherla@gmail.com>
2026-07-22 23:38:05 +0530

    Create div.c

---
+++ div.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+       float a,b,div;
+       
+       printf("Enter a value :");
+       scanf("%f",&a);
+
+       printf("Enter b value :");
+       scanf("%f",&b);
+
+       div =  a/b;
+    
+       printf("Div = %.2f\n",div);
+
+       return 0;
+}
