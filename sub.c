0ed805a4b00407b4600e35750d1bc3f6a771ef44
Pallavi <pallaviyericherla@gmail.com>
2026-07-22 23:22:01 +0530

    Create sub.c

---
+++ sub.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+      int a,b,sub;
+
+      printf("Enter value for a :");
+      scanf("%d",&a);
+
+      printf("Enter value for b :");
+      scanf("%d",&b);
+   
+      sub = a - b;
+
+      printf("Sub = %d\n",sub);
+
+      return 0;
+}
+      
