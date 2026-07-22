53fd4cea3bcab5ce5dee9bc16911bfa20683a723
Pallavi <pallaviyericherla@gmail.com>
2026-07-22 23:28:39 +0530

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+      int a,b,mul;
+   
+      printf("Enter a value :");
+      scanf("%d",&a);
+      
+      printf("Enter b value :");
+      scanf("%d",&b);
+
+      mul = a * b;
+
+      printf("Mul = %d\n",mul);
+   
+      return 0;
+}
+      
+      
